29272129fe5b0c495dee5b58a63a585dd7de7a22
deepu <deepumahapatro2006@gmail.com>
2026-05-27 09:31:23 +0530

    SORT CHARACTERS BY FREQUENCY AND ROTATAE THE STRING

--- DAY18.py
+++ DAY18.py
@@ -84,4 +84,125 @@ def frequency_sort(s):
     return result
 print(frequency_sort("tree"))
     
-    
+#ROTATE THE STRING
+#A ROTATE STRING BEHAVE LIKE A CIRCULAR STRUCTURE
+#AFTER THE LAST CHARACTER THE FIRST CHARACTER COMES AGAIN
+#ROTATION DOES NOT REMOVE ANY CHARACTER AND DOES NOT ADD
+#IT JUST CHANGE THE POSITION OF THE CHARACTER
+#THERE ARE TWO TYPES OF ROTATION:
+            #LEFT ROTATION: FRONT CHARACTERS MOVE TO END
+            #RIGHT ROTATION: END CHARACTERS MOVE FRONT
+#ROTATION MEANS SELECTING A DIFFERENT STARTING POINT IN THE SAME CIRCULAR SEQUENCE
+#EDGE CASES: 
+            # EMPTY STRING -> NO CHANGE
+            #SINGLE CHARACTER -> NO CHANGE
+            #SAME CHARACTERS -> UNCHANGED ROTATION
+            #STRINGS WITH DIFFERENT LENGTHS CANNOT BE ROTATION
+
+#USING SLICING METHOD(LEFT ROTATION)
+def left_rotate(s,k):
+    #EDGE CASE: EMPTY STRING
+    if s=="":
+        return ""
+    #STRING LENGTH
+    n=len(s)
+    #EDGE CASE: HANDLE LARGE ROTATIONS
+    k=k%n
+    #EDGE CASE : ROTATION BY 0
+    if k==0:
+        return s
+    #ROTATION PROCESS
+    return s[k:]+s[:k]
+print(left_rotate("abcde",2))
+
+#USING SLICING METHOD(RIGHT ROTATION)
+def right_rotate(s, k):
+    #EDGE CASE 1: EMPTY STRING
+    if s == "":
+        return ""
+    #STRING LENGTH
+    n = len(s)
+    #EDGE CASE 2: HANDLE LARGE ROTATIONS
+    k = k % n
+    #EDGE CASE 3: ROTATION BY 0
+    if k == 0:
+        return s
+    #ROTATION
+    return s[-k:] + s[:-k]
+print(right_rotate("abcde", 2))
+
+#USING REVERSAL METHOD
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+#LEFT ROTATION USING REVERSAL
+def rotate_reversal(s, k):
+    #EDGE CASE 1: EMPTY STRING
+    if s == "":
+        return ""
+    arr = list(s)
+    n = len(arr)
+    #HANDLE LARGE ROTATIONS
+    k = k % n
+    #EDGE CASE 2: K = 0
+    if k == 0:
+        return s
+    #STEP 1
+    reverse(arr, 0, k - 1)
+    #STEP 2
+    reverse(arr, k, n - 1)
+    #STEP 3
+    reverse(arr, 0, n - 1)
+    return "".join(arr)
+#TEST
+print(rotate_reversal("abcde", 2))
+
+#USING LOOP METHOD CONCEPT(LEFT ROTATE)
+def left_rotate_loop(s, k):
+    #EDGE CASE 1: EMPTY STRING
+    if s == "":
+        return ""
+    n = len(s)
+    #HANDLE LARGE ROTATIONS
+    k = k % n
+    #CONVERT TO LIST
+    s = list(s)
+    #ROTATE ONE BY ONE
+    for i in range(k):
+        #STORE FIRST CHARACTER
+        first = s[0]
+        #SHIFT LEFT
+        for j in range(n - 1):
+            s[j] = s[j + 1]
+        #PLACE FIRST AT END
+        s[n - 1] = first
+    return "".join(s)
+#TEST
+print(left_rotate_loop("abcde", 2))
+
+#USING LOOP METHOD (RIGHT ROTATE)
+def right_rotate_loop(s, k):
+    #EDGE CASE 1: EMPTY STRING
+    if s == "":
+        return ""
+    #STRING LENGTH
+    n = len(s)
+    #HANDLE LARGE ROTATIONS
+    k = k % n
+    #CONVERT STRING TO LIST
+    s = list(s)
+    #ROTATE ONE BY ONE
+    for i in range(k):
+        #STORE LAST CHARACTER
+        last = s[n - 1]
+        #SHIFT RIGHT
+        for j in range(n - 1, 0, -1):
+            s[j] = s[j - 1]
+        #PLACE LAST CHARACTER AT FRONT
+        s[0] = last
+    #CONVERT BACK TO STRING
+    return "".join(s)
+#TEST
+print(right_rotate_loop("abcde", 2))
